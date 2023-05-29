aab9f08ce6073a6497d927b7b7c59c114cad8b0d
RekhaKemburaju <rekha.kemburaju@gmail.com>
2023-05-30 08:42:31 +1000

    MyFirstProjectWithSelenium project
    MyFirstProjectWithSelenium project

---
+++ MyFirstProjectWithSelenium/MyFirstProjectWithSelenium/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MyFirstProjectWithSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com/");
+            driver.FindElement(By.Name("q")).SendKeys("Automation Traning");
+           // driver.Quit();
+        }
+    }
+}
